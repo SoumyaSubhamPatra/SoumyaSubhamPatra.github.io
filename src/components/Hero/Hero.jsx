464a96e412634b6d3bfd9405a3f70c36e372411a
SoumyaSubhamPatra <soumya0299@gmail.com>
2024-08-29 19:08:36 +0530

    CV downloaded updated again

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,51 @@
-import React from "react";
-import styles from "./Hero.module.css";
+// import React from "react";
+// import styles from "./Hero.module.css";
+// import { getImageUrl } from "../../utils";
+// import { saveAs } from "file-saver";
+
+// export const Hero = () => {
+//   const handleDownload = () => {
+//     const pdfUrl =
+//       "https://drive.google.com/file/d/1N4oa_f2wc6hfkLYtcCNOFEJWwRTPnFBp/view?usp=sharing";
+//     const pdfName = "Soumya-Patra-Resume.pdf";
+
+//     saveAs(pdfUrl, pdfName);
+//   };
+
+//   return (
+//     <section className={styles.container}>
+//       <div className={styles.content}>
+//         <h1 className={styles.title}>Hi, I'm Soumya Subham Patra</h1>
+//         <p className={styles.description}>
+//           I'm a full-stack developer with NodeJS. Reach out if you'd like to
+//           learn more!
+//         </p>
+//         <button onClick={handleDownload} className={styles.contactBtn}>
+//           My Resume
+//         </button>
+//       </div>
+//       <img
+//         src={getImageUrl("hero/Logo.png")}
+//         alt="Hero image of me"
+//         className={styles.heroImg}
+//       />
+//       <div className={styles.topBlur} />
+//       <div className={styles.bottomBlur} />
+//     </section>
+//   );
+// };
 import { getImageUrl } from "../../utils";
+import styles from "./Hero.module.css";
+import React from "react";
 import { saveAs } from "file-saver";
 
-export const Hero = () => {
+const Hero = () => {
   const handleDownload = () => {
     const pdfUrl = "../../../assets/resume/resume.pdf"; // URL or path to your PDF file
-    const pdfName = "Soumya-Patra-Resume.pdf"; // Name of the downloaded file
+    const pdfName = "downloaded-file.pdf"; // Name of the downloaded file
 
     saveAs(pdfUrl, pdfName);
   };
-
   return (
     <section className={styles.container}>
       <div className={styles.content}>
@@ -20,7 +55,7 @@ export const Hero = () => {
           learn more!
         </p>
         <button onClick={handleDownload} className={styles.contactBtn}>
-          My Resume
+          Download PDF
         </button>
       </div>
       <img
@@ -33,3 +68,5 @@ export const Hero = () => {
     </section>
   );
 };
+
+export default Hero;
