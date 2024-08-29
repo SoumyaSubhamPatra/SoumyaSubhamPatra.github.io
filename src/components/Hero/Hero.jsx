150b1d90e267422e0236fb28212af2fefa743f74
SoumyaSubhamPatra <soumya0299@gmail.com>
2024-08-29 18:49:32 +0530

    CV downloaded

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
+import { saveAs } from "file-saver";
 
 export const Hero = () => {
+  const handleDownload = () => {
+    const pdfUrl = "../../../assets/resume/resume.pdf"; // URL or path to your PDF file
+    const pdfName = "downloaded-file.pdf"; // Name of the downloaded file
+
+    saveAs(pdfUrl, pdfName);
+  };
+
   return (
     <section className={styles.container}>
       <div className={styles.content}>
@@ -12,12 +19,10 @@ export const Hero = () => {
           I'm a full-stack developer with NodeJS. Reach out if you'd like to
           learn more!
         </p>
-        <a
-          href="https://resume-builder-test-new.masaischool.com/resume/public?resumeId=6676b78942e58acbb8500122"
-          className={styles.contactBtn}
-        >
+        <a href="https://drive.google.com/file/d/1N4oa_f2wc6hfkLYtcCNOFEJWwRTPnFBp/view?usp=sharing"></a>
+        <button onClick={handleDownload} className={styles.contactBtn}>
           My Resume
-        </a>
+        </button>
       </div>
       <img
         src={getImageUrl("hero/Logo.png")}
